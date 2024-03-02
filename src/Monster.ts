8c477a7b5fc6505d87329822baf2515d4cb13da7
rafael <rafaelmagalhaes1@live.com>
2024-03-01 21:56:24 -0300

    feat: Monster class created

---
+++ src/Monster.ts
@@ -0,0 +1,30 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = Math.max(0, attackPoints);
+    this._lifePoints = Math.max(0, this._lifePoints - damage);
+
+    return this._lifePoints <= 0 ? -1 : this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
